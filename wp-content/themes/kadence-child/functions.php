2a1b122582c8ba9877bc6126e986e8f896655105
Mobashir1995 <mobashir2762@gmail.com>
2026-01-24 16:26:27 +0600

    test deployment

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -1,5 +1,6 @@
 <?php
 add_action( 'ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content' );
+
 function add_sitewide_custom_content() {
     echo '<h2>We\'re performing scheduled maintenance to improve your experience.</h2>';
     echo '<p>The site will be back online shortly — thank you for your patience.</p>';
