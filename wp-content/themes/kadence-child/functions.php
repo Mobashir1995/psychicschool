4414ef01232c01a8b4de73b97b3abdfceab08665
Mobashir1995 <mobashir2762@gmail.com>
2026-01-24 18:57:46 +0600

    include child theme css

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+add_action( 'wp_enqueue_scripts', 'kadence_child_enqueue_scripts' );
+function kadence_child_enqueue_scripts() {
+    wp_enqueue_style( 'kadence-child-style', get_stylesheet_uri() );
+}
+
 require_once get_stylesheet_directory() . '/inc/vc-addons/index.php';
 add_action( 'ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content' );
 
