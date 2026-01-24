762542940925fd0620281f5904a727dfb6ee82aa
Mobashir1995 <mobashir2762@gmail.com>
2026-01-24 18:43:24 +0600

    include wpbakery addon register

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -1,7 +1,13 @@
 <?php
+
+require_once get_stylesheet_directory() . '/inc/vc-addons/index.php';
 add_action( 'ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content' );
 
 function add_sitewide_custom_content() {
     echo '<h2>We\'re performing scheduled maintenance to improve your experience.</h2>';
     echo '<p>The site will be back online shortly — thank you for your patience.</p>';
+}
+
+function kadence_child_create_unique_id( $atts ) {
+	return 'module__' . md5( serialize( $atts ) ); //phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.serialize_serialize
 }
