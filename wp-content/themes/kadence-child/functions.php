9efdedfcb05047f6b8c1959a6135dde713225e0c
Mobashir1995 <mobashir2762@gmail.com>
2026-01-30 15:15:45 +0600

    refactor functions.php code

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -3,13 +3,12 @@ define('KADENCE_CHILD_VERSION', time());
 add_action('wp_enqueue_scripts', 'kadence_child_enqueue_scripts');
 function kadence_child_enqueue_scripts()
 {
-
     wp_enqueue_style('kadence-child-style', get_stylesheet_uri(), array(), KADENCE_CHILD_VERSION);
 }
 
 require_once get_stylesheet_directory() . '/inc/vc-addons/index.php';
-add_action('ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content');
 
+add_action('ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content');
 function add_sitewide_custom_content()
 {
     echo '<h2>We\'re performing scheduled maintenance to improve your experience.</h2>';
