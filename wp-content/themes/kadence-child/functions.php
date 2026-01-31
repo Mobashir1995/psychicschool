bea5615c2a5ef530487c03ceea87f3dadb30c905
Mobashir1995 <mobashir2762@gmail.com>
2026-01-31 18:37:00 +0600

    enqueue font awesome

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -1,16 +1,21 @@
 <?php
 define('KADENCE_CHILD_VERSION', time());
-add_action('wp_enqueue_scripts', 'kadence_child_enqueue_scripts');
+
 function kadence_child_enqueue_scripts()
 {
     wp_enqueue_style('kadence-child-style', get_stylesheet_uri(), array(), KADENCE_CHILD_VERSION);
+    wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/vendors/fontawesome/css/all.min.css', array(), KADENCE_CHILD_VERSION, 'all' );
+    wp_enqueue_style( 'fontawesome-shims', get_stylesheet_directory_uri() . '/vendors/fontawesome/css/v4-shims.min.css', array('font-awesome'), KADENCE_CHILD_VERSION, 'all' );
+
 }
+add_action('wp_enqueue_scripts', 'kadence_child_enqueue_scripts');
 
-require_once get_stylesheet_directory() . '/inc/vc-addons/index.php';
 
-add_action('ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content');
 function add_sitewide_custom_content()
 {
     echo '<h2>We\'re performing scheduled maintenance to improve your experience.</h2>';
     echo '<p>The site will be back online shortly — thank you for your patience.</p>';
 }
+add_action('ppw_sitewide_above_password_form_container', 'add_sitewide_custom_content');
+
+require_once get_stylesheet_directory() . '/inc/vc-addons/index.php';
