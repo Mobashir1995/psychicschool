fd6e707a4c1e0fa860480fdff96d9b330dd0848d
Mobashir1995 <mobashir2762@gmail.com>
2026-02-06 19:42:07 +0600

    remove kadence wc action wrap start end markup

--- wp-content/themes/kadence-child/inc/components/woocommerce/component.php
+++ wp-content/themes/kadence-child/inc/components/woocommerce/component.php
@@ -46,6 +46,9 @@ function remove_kadence_woocommerce_component() {
 	remove_filter( 'kadence_blocks_carousel_woocommerce_product_loop_start', array( $woocommerce_component, 'product_loop_start' ), 5 );
 	remove_filter( 'post_class', array( $woocommerce_component, 'add_woo_entry_classes' ), 20 );
 	remove_filter( 'product_cat_class', array( $woocommerce_component, 'add_woo_cat_entry_classes' ), 20 );
+    // Add to cart wrap.
+    remove_action( 'woocommerce_after_shop_loop_item', array( $woocommerce_component, 'archive_action_wrap_start' ), 5 );
+	remove_action( 'woocommerce_after_shop_loop_item', array( $woocommerce_component, 'archive_action_wrap_end' ), 20 );
 	
 	// Remove the component from the components array
 	// unset( $theme->components['woocommerce'] );
