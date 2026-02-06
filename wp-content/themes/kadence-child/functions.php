8a3616917b72cb758b01383fd43835a935d79703
Mobashir1995 <mobashir2762@gmail.com>
2026-02-06 19:23:23 +0600

    move wpbakery components under the components folder

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -10,9 +10,8 @@ function kadence_child_enqueue_scripts()
 }
 add_action('wp_enqueue_scripts', 'kadence_child_enqueue_scripts');
 
-require_once get_stylesheet_directory() . '/inc/vc-addons/index.php';
-
 $kadence_child_components = array(
+    'wpbakery',
     'password-protection',
     'woocommerce',
 );
