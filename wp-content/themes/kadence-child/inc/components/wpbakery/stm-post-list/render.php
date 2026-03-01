44c9687add3564ba0f7373e6d9753b99de2af966
Mobashir1995 <mobashir2762@gmail.com>
2026-03-01 14:58:25 +0600

    Refactor STM Post List rendering for improved layout and responsiveness
    - Updated the rendering logic to utilize a responsive grid system for post items.
    - Enhanced the structure to support Bootstrap classes for better alignment and spacing.
    - Maintained existing features such as post thumbnails, dates, comments, categories, and tags while improving overall layout consistency.

--- wp-content/themes/kadence-child/inc/components/wpbakery/stm-post-list/render.php
+++ wp-content/themes/kadence-child/inc/components/wpbakery/stm-post-list/render.php
@@ -51,73 +51,86 @@ function kadence_child_stm_post_list_render( $atts ) {
 	if ( $query->have_posts() ) :
 		?>
 		<div class="kadence_post_list_main_section_wrapper <?php echo esc_attr( $rand_class ); ?>">
-			<?php
-			while ( $query->have_posts() ) :
-				$query->the_post();
-				?>
-				<div class="kadence_post_list_content_unit">
-					<?php if ( has_post_thumbnail() ) : ?>
-						<div class="kadence_post_list_featured_image">
-							<a href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'View post details', 'kadence-child' ); ?>">
-								<?php the_post_thumbnail( 'large', array( 'class' => 'img-responsive' ) ); ?>
-							</a>
-						</div>
-					<?php endif; ?>
-
-					<?php if ( $post_list_show_date ) : ?>
-						<div class="kadence_post_list_meta_unit">
-							<div class="kadence_post_list_date_d"><?php echo esc_html( get_the_date( 'd' ) ); ?></div>
-							<div class="kadence_post_list_date_m"><?php echo esc_html( get_the_date( 'M' ) ); ?></div>
-							<?php if ( $post_list_show_comments ) : ?>
-								<?php $comments_num = get_comments_number( get_the_ID() ); ?>
-								<?php if ( $comments_num ) : ?>
-									<div class="kadence_post_list_comment_num">
-										<span><?php echo esc_html( $comments_num ); ?></span><i class="fa-icon-stm_icon_comment_o"></i>
+			<div class="row">
+				<?php
+				while ( $query->have_posts() ) :
+					$query->the_post();
+					++$current_list_item;
+					?>
+					<div class="col-md-<?php echo esc_attr( $post_list_item_col ); ?> col-sm-<?php echo esc_attr( $post_list_item_col ); ?> col-xs-12">
+						<div class="kadence_post_list_content_unit">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="kadence_post_list_featured_image">
+									<a href="<?php the_permalink(); ?>" title="<?php esc_attr_e( 'View post details', 'kadence-child' ); ?>">
+										<?php the_post_thumbnail( 'large', array( 'class' => 'img-responsive' ) ); ?>
+									</a>
+								</div>
+							<?php endif; ?>
+							<div class="row">
+								<?php if ( $post_list_show_date ) : ?>
+									<div class="col-md-3 col-sm-4 col-xs-12">
+										<div class="kadence_post_list_meta_unit">
+											<div class="kadence_post_list_date_d"><?php echo esc_html( get_the_date( 'd' ) ); ?></div>
+											<div class="kadence_post_list_date_m"><?php echo esc_html( get_the_date( 'M' ) ); ?></div>
+											<?php if ( $post_list_show_comments ) : ?>
+												<?php $comments_num = get_comments_number( get_the_ID() ); ?>
+												<?php if ( $comments_num ) : ?>
+													<div class="kadence_post_list_comment_num">
+														<span><?php echo esc_html( $comments_num ); ?></span><i class="fa-icon-stm_icon_comment_o"></i>
+													</div>
+												<?php endif; ?>
+											<?php endif; ?>
+											<?php if ( is_sticky( get_the_ID() ) ) : ?>
+												<div class="kadence_post_list_sticky_post heading_font"><?php esc_html_e( 'Sticky Post', 'kadence-child' ); ?></div>
+											<?php endif; ?>
+										</div>
 									</div>
 								<?php endif; ?>
-							<?php endif; ?>
-							<?php if ( is_sticky( get_the_ID() ) ) : ?>
-								<div class="kadence_post_list_sticky_post heading_font"><?php esc_html_e( 'Sticky Post', 'kadence-child' ); ?></div>
-							<?php endif; ?>
+								<div class="<?php echo $post_list_show_date ? 'col-md-9 col-sm-8 col-xs-12' : 'col-md-12'; ?>">
+									<div class="kadence_post_list_inner_content_unit <?php echo $post_list_show_date ? 'kadence_post_list_inner_content_unit_left' : ''; ?>">
+										<a href="<?php the_permalink(); ?>" class="kadence_post_list_item_title h3"><?php the_title(); ?></a>
+										<div class="kadence_post_list_item_excerpt"><?php the_excerpt(); ?></div>
+										<div class="kadence_post_list_short_separator"></div>
+
+										<?php if ( $post_list_show_cats ) : ?>
+											<?php
+											$post_list_item_categories = wp_get_post_categories( get_the_ID() );
+											if ( ! empty( $post_list_item_categories ) ) :
+												?>
+												<div class="kadence_post_list_cats">
+													<span class="kadence_post_list_cats_label"><?php esc_html_e( 'Posted in:', 'kadence-child' ); ?></span>
+													<?php foreach ( $post_list_item_categories as $post_list_single_cat ) : ?>
+														<?php $post_list_cat = get_category( $post_list_single_cat ); ?>
+														<a href="<?php echo esc_url( get_term_link( $post_list_cat ) ); ?>"><?php echo esc_html( $post_list_cat->name ); ?></a><span class="kadence_post_list_divider">,</span>
+													<?php endforeach; ?>
+												</div>
+											<?php endif; ?>
+										<?php endif; ?>
+
+										<?php if ( $post_list_show_tags ) : ?>
+											<?php
+											$posttags = get_the_tags();
+											if ( $posttags ) :
+												?>
+												<div class="kadence_post_list_item_tags">
+													<span class="kadence_post_list_tags_label"><?php esc_html_e( 'Tags:', 'kadence-child' ); ?></span>
+													<?php foreach ( $posttags as $tag_object ) : ?>
+														<a href="<?php echo esc_url( get_tag_link( $tag_object->term_id ) ); ?>"><?php echo esc_html( $tag_object->name ); ?></a><span class="kadence_post_list_divider">,</span>
+													<?php endforeach; ?>
+												</div>
+											<?php endif; ?>
+										<?php endif; ?>
+									</div>
+								</div>
+							</div>
 						</div>
-					<?php endif; ?>
-
-					<div class="kadence_post_list_inner_content_unit <?php echo $post_list_show_date ? 'kadence_post_list_inner_content_unit_left' : ''; ?>">
-						<a href="<?php the_permalink(); ?>" class="kadence_post_list_item_title h3"><?php the_title(); ?></a>
-						<div class="kadence_post_list_item_excerpt"><?php the_excerpt(); ?></div>
-						<div class="kadence_post_list_short_separator"></div>
-
-						<?php if ( $post_list_show_cats ) : ?>
-							<?php
-							$post_list_item_categories = wp_get_post_categories( get_the_ID() );
-							if ( ! empty( $post_list_item_categories ) ) :
-								?>
-								<div class="kadence_post_list_cats">
-									<span class="kadence_post_list_cats_label"><?php esc_html_e( 'Posted in:', 'kadence-child' ); ?></span>
-									<?php foreach ( $post_list_item_categories as $post_list_single_cat ) : ?>
-										<?php $post_list_cat = get_category( $post_list_single_cat ); ?>
-										<a href="<?php echo esc_url( get_term_link( $post_list_cat ) ); ?>"><?php echo esc_html( $post_list_cat->name ); ?></a><span class="kadence_post_list_divider">,</span>
-									<?php endforeach; ?>
-								</div>
-							<?php endif; ?>
-						<?php endif; ?>
-
-						<?php if ( $post_list_show_tags ) : ?>
-							<?php
-							$posttags = get_the_tags();
-							if ( $posttags ) :
-								?>
-								<div class="kadence_post_list_item_tags">
-									<span class="kadence_post_list_tags_label"><?php esc_html_e( 'Tags:', 'kadence-child' ); ?></span>
-									<?php foreach ( $posttags as $tag_object ) : ?>
-										<a href="<?php echo esc_url( get_tag_link( $tag_object->term_id ) ); ?>"><?php echo esc_html( $tag_object->name ); ?></a><span class="kadence_post_list_divider">,</span>
-									<?php endforeach; ?>
-								</div>
-							<?php endif; ?>
-						<?php endif; ?>
 					</div>
-				</div>
-			<?php endwhile; ?>
+					<?php if ( 0 === $current_list_item % max( 1, $post_list_per_row ) ) : ?>
+						</div>
+						<div class="row">
+					<?php endif; ?>
+				<?php endwhile; ?>
+			</div>
 		</div>
 		<?php
 		wp_reset_postdata();
