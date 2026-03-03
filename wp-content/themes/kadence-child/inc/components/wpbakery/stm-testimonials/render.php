634e7c9ee8ea234adfea6608171f15d703d07e02
Mobashir1995 <mobashir2762@gmail.com>
2026-03-03 16:37:05 +0600

    Refactor testimonials rendering in Kadence Child theme
    - Moved the testimonial content HTML structure to ensure the excerpt is displayed correctly after the author and profession details for improved readability.

--- wp-content/themes/kadence-child/inc/components/wpbakery/stm-testimonials/render.php
+++ wp-content/themes/kadence-child/inc/components/wpbakery/stm-testimonials/render.php
@@ -53,14 +53,14 @@ function kadence_child_stm_testimonials_render( $atts ) {
 					?>
 					<div class="swiper-slide">
 						<div class="kadence_testimonial_item" style="color:<?php echo esc_attr( $text_color ); ?>;">
-							<div class="kadence_testimonial_content">
-								<?php the_excerpt(); ?>
-							</div>
 							<div class="kadence_testimonial_meta">
 								<div class="kadence_testimonial_author"><?php the_title(); ?></div>
 								<?php if ( $profession ) : ?>
 									<div class="kadence_testimonial_profession"><?php echo esc_html( $profession ); ?></div>
 								<?php endif; ?>
+							</div>
+							<div class="kadence_testimonial_content">
+								<?php the_excerpt(); ?>
 							</div>
 						</div>
 					</div>
