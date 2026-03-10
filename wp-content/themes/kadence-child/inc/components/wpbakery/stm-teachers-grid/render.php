8967e10a6e5d8c170efc175b302404ef90583c7a
Mobashir1995 <mobashir2762@gmail.com>
2026-03-10 16:41:16 +0600

    Add pagination styles and update rendering in STM Teachers Grid
    - Introduced new CSS styles for pagination, improving layout and usability of page numbers.
    - Updated pagination rendering in the STM Teachers Grid to include previous and next text for enhanced navigation experience.

--- wp-content/themes/kadence-child/inc/components/wpbakery/stm-teachers-grid/render.php
+++ wp-content/themes/kadence-child/inc/components/wpbakery/stm-teachers-grid/render.php
@@ -100,6 +100,8 @@ function kadence_child_stm_teachers_grid_render( $atts ) {
 							'total'   => $query->max_num_pages,
 							'current' => $paged,
 							'type'    => 'list',
+							'prev_text'          => __( '&laquo;' ),
+							'next_text'          => __( '&raquo;' ),
 						)
 					)
 				);
