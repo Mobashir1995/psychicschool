b985db8298b522fe5a716849617ea68c31ff3b7c
Mobashir1995 <mobashir2762@gmail.com>
2026-03-27 12:50:30 +0600

    Enhance teacher name styling in STM teachers grid component
    - Added 'heading-font' class to the teacher name div for improved typography consistency.
    - Ensured better visual integration with the overall theme design.

--- wp-content/themes/kadence-child/inc/components/wpbakery/stm-teachers-grid/render.php
+++ wp-content/themes/kadence-child/inc/components/wpbakery/stm-teachers-grid/render.php
@@ -73,7 +73,7 @@ function kadence_child_stm_teachers_grid_render( $atts ) {
 							</div>
 						</a>
 						<div class="teacher-info">
-							<div class="teacher-name">
+							<div class="teacher-name heading-font">
 								<a href="<?php echo esc_url( get_permalink( $teacher_id ) ); ?>" title="<?php echo esc_html( get_the_title( $teacher_id ) ); ?>"><?php echo esc_html( get_the_title( $teacher_id ) ); ?></a>
 							</div>
 							<?php if ( ! empty( $position ) ) : ?>
