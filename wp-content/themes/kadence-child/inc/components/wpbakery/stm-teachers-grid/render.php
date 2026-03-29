ce5e9e4363ebbf59413dcbb072976acab2270aca
Mobashir1995 <mobashir2762@gmail.com>
2026-03-29 19:20:47 +0600

    Refactor teacher role styling and HTML structure for improved semantics
    - Removed font-size property from the CSS for teacher roles to streamline styling.
    - Updated HTML structure for teacher roles to use a <div> with 'h6' class for enhanced semantic clarity and consistency across components.

--- wp-content/themes/kadence-child/inc/components/wpbakery/stm-teachers-grid/render.php
+++ wp-content/themes/kadence-child/inc/components/wpbakery/stm-teachers-grid/render.php
@@ -72,16 +72,17 @@ function kadence_child_stm_teachers_grid_render( $atts ) {
 								?>
 							</div>
 						</a>
-						<div>
-							<h4 class="teacher-name">
-								<a href="<?php echo esc_url( get_permalink( $teacher_id ) ); ?>" title="<?php echo esc_html( get_the_title( $teacher_id ) ); ?>"><?php echo esc_html( get_the_title( $teacher_id ) ); ?></a>
-							</h4>
-							<?php if ( ! empty( $position ) ) : ?>
-								<div class="teacher-role">
-									<?php echo esc_html( $position ); ?>
-								</div>
-							<?php endif; ?>
-						</div>
+						
+						<h4 class="teacher-name">
+							<a href="<?php echo esc_url( get_permalink( $teacher_id ) ); ?>" title="<?php echo esc_html( get_the_title( $teacher_id ) ); ?>"><?php echo esc_html( get_the_title( $teacher_id ) ); ?></a>
+						</h4>
+						
+						<?php if ( ! empty( $position ) ) : ?>
+							<div class="teacher-role h6">
+								<?php echo esc_html( $position ); ?>
+							</div>
+						<?php endif; ?>
+						
 						<div class="teacher-content">
 							<?php the_excerpt(); ?>
 						</div>
